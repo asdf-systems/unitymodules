b33d5be6af22074a4af1f39634cebca5155749a4
Felix <fedietz@gmail.com>
2011-11-09 17:41:34 +0100

    edited review changes

--- Assets/scripts/MouseFollowBehaviour.cs
+++ Assets/scripts/MouseFollowBehaviour.cs
@@ -3,9 +3,7 @@ using System.Collections;
 
 public class MouseFollowBehaviour : MonoBehaviour {
 	
-	public Vector3 screenPosition;
 	public Vector3 mousePosition;
-	private float distanceAdjustment;
 	private Plane plane;
 	private Ray ray;
 	private float raycast;
@@ -21,30 +19,14 @@ public class MouseFollowBehaviour : MonoBehaviour {
 	void Update () {
 	}
 	
-	//Store the screen position of the object when the mouse clicks
-	private void OnMouseDown()
-	{
-		
-	}
-	
 	//When the mouse drags, change the object's screen position accordingly.
 	private void OnMouseDrag()
 	{
 		plane = new Plane((Camera.mainCamera.transform.forward).normalized, this.transform.position);
 		mousePosition = Input.mousePosition;
 		ray = Camera.mainCamera.ScreenPointToRay(mousePosition);
-		Debug.Log(ray.origin + " "+ ray.direction + " ");
-		Debug.Log(mousePosition);
 		plane.Raycast(ray, out raycast);
-		Debug.Log(raycast);
-		Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
-		translationPoint = ray.GetPoint(raycast);
-		Debug.Log("Translationpoint: "+translationPoint);
-		
-	    this.transform.position = translationPoint;
-	}
-	
-	private void SetResAdjustments(){
-		
+		translationPoint = ray.GetPoint(raycast);		
+	    gameObject.transform.position = translationPoint;
 	}
 }
